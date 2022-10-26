099cdf8e645b038953026dd41c06ffd99bbb8311
Helgy <oleglavro6@gmail.com>
2022-10-26 10:14:17 +0300

    staticruntime off

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -8,6 +8,9 @@
 	#endif
 #endif 
 
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif
 
 #ifdef HZ_ENABLE_ASSERTS
 	#define HZ_ASSERT(x,...) {if(!x){HZ_ERROR("Assertion Failed: {0}",__VA_ARGS__);__debugbreak();}}
